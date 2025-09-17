285c220d269407d0f4a0008b86da02fc9d831c05
Victor Pacheco <v.emanuel.pacheco@gmail.com>
2025-09-17 08:20:45 -0300

    v2.0 release

--- Agenda TAB/Core/Domain/Repository/MoodRepository.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Combine
-
-protocol MoodRepository {
-    func getDailyEntry(for date: Date) async -> DailyEntry?
-    func getDailyEntries(from startDate: Date, to endDate: Date) -> AnyPublisher<[DailyEntry], Error>
-    func saveDailyEntry(_ entry: DailyEntry) async throws
-    func deleteDailyEntry(for date: Date) async throws
-}
